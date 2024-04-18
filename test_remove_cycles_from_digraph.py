a2d2444ff96db174734a1c0dfce990fbf96aef31
Michael Russell <37958894+michaelrussell4@users.noreply.github.com>
2024-04-18 15:24:53 -0600

    Added DiGraph engine to create_using

--- test_remove_cycles_from_digraph.py
+++ test_remove_cycles_from_digraph.py
@@ -2,6 +2,6 @@ import networkx as nx
 from cycle_remover import remove_cycles_from_digraph
 
 def test_remove_cycles_from_digraph():
-    G = nx.read_edgelist('cyclic_digraph.edges')
+    G = nx.read_edgelist('cyclic_digraph.edges', create_using=nx.DiGraph())
     DAG = remove_cycles_from_digraph(G)
     assert nx.is_directed_acyclic_graph(DAG)
